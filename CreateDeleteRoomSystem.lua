edbeedfeb55e18b5d60cf55dcdb61e95d02d88ec
djeku123441 <99770925+djeku123441@users.noreply.github.com>
2025-02-12 01:59:23 +0100

    Update CreateDeleteRoomSystem.lua

--- CreateDeleteRoomSystem.lua
+++ CreateDeleteRoomSystem.lua
@@ -2,14 +2,44 @@ game.ReplicatedStorage.Events.CreateUI.OnServerEvent:Connect(function(player,plr
 	print(player)
 	print(plr)
 	game.ReplicatedStorage.Events.CreateUI:FireAllClients(plr)
-	local hum = player.Character:WaitForChild("HumanoidRootPart")
-	hum.CFrame = CFrame.new(game.Workspace.WaitingRoom.Position)
-	game.ReplicatedStorage.Events.DisableControls:FireClient(player)
+	for i,v in pairs(game.Workspace.Rooms:GetChildren()) do
+		for s,k in pairs(v:GetChildren()) do
+			print(v)
+			if k:IsA("BoolValue") then
+				print(k)
+				if k.Value == false then
+					print(k)
+					print(v)
+					local hum = player.Character:WaitForChild("HumanoidRootPart")
+					hum.CFrame = CFrame.new(game.Workspace.Rooms:FindFirstChild(v.Name).WaitingRoom.Position)
+					v.taken.Value = true
+					v.id.Value = player.UserId
+					print(player.UserId)
+					game.ReplicatedStorage.Events.DisableControls:FireClient(player)
+					return end
+			end
+		end
+	end
 end)
 
 game.ReplicatedStorage.Events.DeleteRoom.OnServerEvent:Connect(function(player,id)
 	print(id)
+	print(player)
 	game.ReplicatedStorage.Events.DeleteRoom:FireAllClients(id)
 	local hum = player.Character:WaitForChild("HumanoidRootPart")
 	hum.CFrame = CFrame.new(game.Workspace.SpawnLocation.Position)
+	game.ReplicatedStorage.Events.DisableControls:FireClient(player)
+	for i,v in pairs(game.Workspace.Rooms:GetChildren()) do
+		for s,k in pairs(v:GetChildren()) do
+			print(v)
+			print(k)
+			if k:IsA("IntValue") and k.Value == player.UserId then
+				print(k)
+				print(k.Value)
+				print("id")
+				k.Value = 0
+				v.taken.Value = false
+				return end
+		end
+	end
 end)
